e12417fbf597d8c832f7ec5adf80af63cc701b79
wilhamsJW <wilhamsmeira@gmail.com>
2020-07-08 12:05:49 -0300

    form interface and sent data via post

--- index.php
+++ index.php
@@ -8,34 +8,62 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>Olá, mundo!</title>
+    <!-- Link Css estilizado -->
+    <link rel="stylesheet" href="css_style/style.css">
+
+    <title>Formulário de Cadastro</title>
+
   </head>
   <body>
-    <h1>Olá, mundo!</h1>
+   
+  <div class="all">
+  <div class="container" id="containerprincipal">
+  <form action="_inserir_produto.php" method="post" id="form">
+  <h4 id="h4">Formulário de Cadastro</h4>  
+  <div class="form-group">
+    <label for="exampleInputEmail1">Número do Produto</label>
+    <input type="number" class="form-control" name="numeroproduto" aria-describedby="emailHelp" placeholder="Insira o número do produto" require>
+    </div>
 
+    <div class="form-group">
+    <label for="exampleInputEmail1">Nome do Produto</label>
+    <input type="text" class="form-control" name="nomeproduto" aria-describedby="emailHelp" placeholder="Insira o nome do produto" require>
+    </div>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Destaques</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Preços</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Desativado</a>
-      </li>
-    </ul>
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">Categoria</label>
+    <select class="form-control" name="categoria" require>
+      <option>Periférico</option>
+      <option>Hardware</option>
+      <option>Software</option>
+      <option>Celulares</option>
+    </select>
   </div>
-</nav>
+  <div class="form-group">
+
+  
+  <div class="form-group">
+    <label for="exampleInputEmail1">Quantidade</label>
+    <input type="number" class="form-control" name="quantidade"  aria-describedby="emailHelp" placeholder="Insira a quantidade do produto" require>
+    </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Fornecedor</label>
+    <select class="form-control" name="fornecedor">
+      <option>Fornecedor A</option>
+      <option>Fornecedor B</option>
+      <option>Fornecedor C</option>
+    </select>
+  </div>
+  <div class="form-group">
+
+  <div style="text-align: right;">
+  <button type="submit" class="btn btn-success btn-sm" id="button">Cadastrar</button>
+  </div>
+
+</div>
+</div>
+</form>
 
 
     <!-- JavaScript (Opcional) -->
