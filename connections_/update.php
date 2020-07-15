b83b0a5725d0c40aa3d943ef908094f613f23800
wilhamsJW <wilhamsmeira@gmail.com>
2020-07-15 14:29:29 -0300

    fix update file, number_int for special_chars

--- connections_/update.php
+++ connections_/update.php
@@ -1,27 +1,25 @@
-<?php 
+<?php
 include_once 'connection.php';
 
-if(isset($_POST['id'])){
+if (isset($_POST['id'])) {
 
-$id            = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
-//$numeroproduto = filter_input(INPUT_POST, 'numeroproduto', FILTER_SANITIZE_NUMBER_INT); o usuário não pode alterar o número
-$nomeproduto   = filter_input(INPUT_POST, 'nomeproduto', FILTER_SANITIZE_SPECIAL_CHARS);
-$categoria     = filter_input(INPUT_POST, 'categoria', FILTER_SANITIZE_NUMBER_INT);
-$quantidade    = filter_input(INPUT_POST, 'quantidade', FILTER_SANITIZE_NUMBER_INT);
-$fornecedor    = filter_input(INPUT_POST, 'fornecedor', FILTER_SANITIZE_SPECIAL_CHARS);
+    $id            = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+    //$numeroproduto = filter_input(INPUT_POST, 'numeroproduto', FILTER_SANITIZE_NUMBER_INT); o usuário não pode alterar o número
+    $nomeproduto   = filter_input(INPUT_POST, 'nomeproduto', FILTER_SANITIZE_SPECIAL_CHARS);
+    $categoria     = filter_input(INPUT_POST, 'categoria', FILTER_SANITIZE_SPECIAL_CHARS);
+    $quantidade    = filter_input(INPUT_POST, 'quantidade', FILTER_SANITIZE_NUMBER_INT);
+    $fornecedor    = filter_input(INPUT_POST, 'fornecedor', FILTER_SANITIZE_SPECIAL_CHARS);
 
-$sql = "UPDATE `estoque` SET `nomeproduto`='$nomeproduto',`categoria`='$categoria',`quantidade`='$quantidade',`fornecedor`='$fornecedor' WHERE id_estoque = $id";
-$query = mysqli_query($conecta, $sql);
+    $sql = "UPDATE `estoque` SET `nomeproduto`='$nomeproduto',`categoria`='$categoria',`quantidade`='$quantidade',`fornecedor`='$fornecedor' WHERE id_estoque = $id";
+    $query = mysqli_query($conecta, $sql);
 
-$info = mysqli_fetch_assoc($query);
+    $info = mysqli_fetch_assoc($query);
 
-if(!empty($info)){
-    echo "Erro na Alteração!";
-    header("location:../listar_produtos.php");
-} else {
-    $mensagem = "Alterado Com Sucesso";
-    header("location:../listar_produtos.php");
- }
+    if (!empty($info)) {
+        echo "Erro na Alteração!";
+        header("location:../listar_produtos.php");
+    } else {
+        $mensagem = "Alterado Com Sucesso";
+        header("location:../listar_produtos.php");
+    }
 }
-
-?>
--- listar_produtos.php
+++ listar_produtos.php
@@ -2,59 +2,61 @@
 
 
 <div class="container" style="margin-top: 40px;">
-<h4>Lista de Produtos</h4>
+  <h4>Lista de Produtos</h4>
 
-<!--Exibir mensagem de erro-->
-<?php if (isset($mensagem)){ ?> <!--isset verifica se a var está configurada ou definida, se tiver definida é pq deu falha pois já vem do arquivo loginsenha, se não tiver configurada é pq foi um sucesso-->
+  <!--Exibir mensagem de erro-->
+  <?php if (isset($mensagem)) { ?>
+    <!--isset verifica se a var está configurada ou definida, se tiver definida é pq deu falha pois já vem do arquivo loginsenha, se não tiver configurada é pq foi um sucesso-->
     <div class="alert" style="text-align: center" role="alert">
-        <?php echo $mensagem ?> 
+      <?php echo $mensagem ?>
     </div>
-  <?php } ?> <!--lembre se do fechamento da chave nessa outra tag html-->
+  <?php } ?>
+  <!--lembre se do fechamento da chave nessa outra tag html-->
 
-<table class="table table-striped">
-  <thead>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+
+        <th scope="col">Número do Produto</th>
+        <th scope="col">Nome do Produto</th>
+        <th scope="col">Categoria</th>
+        <th scope="col">Quantidade</th>
+        <th scope="col">Fornecedor</th>
+        <th scope="col">Ação</th>
+      </tr>
+    </thead>
+    <tr>
+      <?php
+      include_once 'connections_/connection.php';
+      $sql = "SELECT * FROM estoque";
+      $query = mysqli_query($conecta, $sql);
+
+      while ($array = mysqli_fetch_assoc($query)) {
+
+        $id_estoque = $array['id_estoque'];
+        $numeroproduto = $array['numeroproduto'];
+        $nomeproduto = $array['nomeproduto'];
+        $categoria = $array['categoria'];
+        $quantidade = $array['quantidade'];
+        $fornecedor = $array['fornecedor'];
+
+      ?>
     <tr>
 
-      <th scope="col">Número do Produto</th>
-      <th scope="col">Nome do Produto</th>
-      <th scope="col">Categoria</th>
-      <th scope="col">Quantidade</th>
-      <th scope="col">Fornecedor</th>
-      <th scope="col">Ação</th>
+      <td><?php echo $numeroproduto ?></td>
+      <td><?php echo $nomeproduto ?></td>
+      <td><?php echo $categoria ?></td>
+      <td><?php echo $quantidade ?></td>
+      <td><?php echo $fornecedor ?></td>
+      <td><a class="btn btn-warning btn-sm" style="color:white;" href="edite.php?id=<?php echo $id_estoque ?>" role="button"><i class="fas fa-edit"></i>&nbsp;Editar</a></td>
+
     </tr>
-  </thead>
-    <tr>
-    <?php
-        include_once 'connections_/connection.php';
-        $sql = "SELECT * FROM estoque";
-        $query = mysqli_query($conecta, $sql);
 
-        while($array = mysqli_fetch_assoc($query)){
+  <?php  } ?>
+  </tr>
 
-            $id_estoque = $array['id_estoque'];
-            $numeroproduto = $array['numeroproduto'];
-            $nomeproduto = $array['nomeproduto'];
-            $categoria = $array['categoria'];
-            $quantidade = $array['quantidade'];
-            $fornecedor = $array['fornecedor'];
 
-       ?>
-           <tr> 
-
-            <td><?php echo $numeroproduto?></td>
-            <td><?php echo $nomeproduto?></td>
-            <td><?php echo $categoria?></td>
-            <td><?php echo $quantidade?></td>
-            <td><?php echo $fornecedor?></td>
-            <td><a class="btn btn-warning btn-sm" style="color:white;" href="edite.php?id=<?php echo $id_estoque ?>" role="button"><i class="fas fa-edit"></i>&nbsp;Editar</a></td>
-
-           </tr> 
-
-    <?php  } ?>
-    </tr>
- 
-
-</table>
+  </table>
 </div>
 
 
